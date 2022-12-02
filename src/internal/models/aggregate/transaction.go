79572933be12257772c874b3e6285a4f373b0b34
Даниил <dan.demin@gmail.com>
2022-12-02 15:42:02 +0300

    add operation type; add method to check if can execute

--- src/internal/models/aggregate/transaction.go
+++ src/internal/models/aggregate/transaction.go
@@ -14,29 +14,44 @@ const (
 	TransactionStatusDenied
 )
 
+type OperationType int
+
+const (
+	OperationTypeAdd OperationType = iota
+	OperationTypeSub
+)
+
 type Transaction struct {
-	ID         string
-	Balance    *entity.Balance
-	Amount     float64
-	Status     TransactionStatus
-	CreatedAt  time.Time
-	ExecutedAt time.Time
+	ID            string
+	Balance       *entity.Balance
+	Amount        float64
+	Status        TransactionStatus
+	OperationType OperationType
+	CreatedAt     time.Time
+	ExecutedAt    time.Time
 }
 
 func NewTransaction(
 	amount float64,
+	operationType OperationType,
 	balance *entity.Balance,
 ) Transaction {
 	return Transaction{
-		Balance:   balance,
-		Amount:    amount,
-		Status:    TransactionStatusCreated,
-		CreatedAt: time.Now().UTC(),
+		Balance:       balance,
+		Amount:        amount,
+		Status:        TransactionStatusCreated,
+		OperationType: operationType,
+		CreatedAt:     time.Now().UTC(),
 	}
 }
 
 func (t Transaction) Execute() {
-	t.Balance.Balance += t.Amount
+	switch t.OperationType {
+	case OperationTypeAdd:
+		t.Balance.Balance += t.Amount
+	case OperationTypeSub:
+		t.Balance.Balance -= t.Amount
+	}
 	t.ExecutedAt = time.Now().UTC()
 }
 
@@ -46,4 +61,12 @@ func (t *Transaction) SetDenied() {
 
 func (t *Transaction) SetSuccess() {
 	t.Status = TransactionStatusSuccess
+}
+
+func (t Transaction) CanExecute() bool {
+	if t.OperationType == OperationTypeSub && t.Balance.Balance < t.Amount {
+		return false
+	}
+
+	return true
 }
