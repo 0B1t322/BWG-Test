7314d2771c5d9abfef9d1669209b9640d64ad6fa
Даниил <dan.demin@gmail.com>
2022-12-02 18:03:33 +0300

    update edges; add mappings; change userid field type

--- src/internal/infrastructure/dal/gorm/models/transcaction.go
+++ src/internal/infrastructure/dal/gorm/models/transcaction.go
@@ -3,7 +3,9 @@ package models
 import (
 	"time"
 
+	"github.com/0B1t322/BWG-Test/internal/models/aggregate"
 	"github.com/google/uuid"
+	"github.com/samber/lo"
 )
 
 const TransactionTableName = "Transactions"
@@ -24,21 +26,46 @@ const (
 	TransactionFieldAmount     TransactionField = `"Amount"`
 	TransactionFieldCreatedAt  TransactionField = `"CreatedAt"`
 	TransactionFieldExecutedAt TransactionField = `"ExecutedAt"`
+	TransactionFieldStatus     TransactionField = `"Status"`
 )
 
 type Transaction struct {
 	ID         uuid.UUID `gorm:"column:Id;type:uuid;primaryKey"`
-	UserID     string    `gorm:"column:UserId;type:uuid;"`
+	UserID     uuid.UUID `gorm:"column:UserId;type:uuid;"`
 	Amount     float64   `gorm:"column:Amount;type:double precision;"`
 	Status     int       `gorm:"column:Status;type:int;"`
 	CreatedAt  time.Time `gorm:"column:CreatedAt;type:timestamp;"`
 	ExecutedAt time.Time `gorm:"column:ExecutedAt;type:timestamp;"`
 
 	// Edges
-	User    *User      `gorm:"foreignKey:UserId;references:Id"`
-	Balance []*Balance `gorm:"foreignKey:UserId"`
+	User    *User    `gorm:"foreignKey:UserId;references:Id"`
+	Balance *Balance `gorm:"foreignKey:UserId"`
 }
 
 func (Transaction) TableName() string {
 	return TransactionTableName
+}
+
+func TransactionModelFrom(transaction aggregate.Transaction) Transaction {
+	return Transaction{
+		ID:         transaction.ID,
+		UserID:     transaction.Balance.UserID,
+		Amount:     transaction.Amount,
+		Status:     transaction.Status.Int(),
+		CreatedAt:  transaction.CreatedAt,
+		ExecutedAt: transaction.ExecutedAt,
+
+		Balance: lo.ToPtr(BalanceModelFrom(transaction.Balance)),
+	}
+}
+
+func TransactionModelTo(transaction *Transaction) aggregate.Transaction {
+	return aggregate.Transaction{
+		ID:         transaction.ID,
+		Balance:    lo.ToPtr(BalanceModelTo(transaction.Balance)),
+		Amount:     transaction.Amount,
+		Status:     aggregate.TransactionStatus(transaction.Status),
+		CreatedAt:  transaction.CreatedAt,
+		ExecutedAt: transaction.ExecutedAt,
+	}
 }
