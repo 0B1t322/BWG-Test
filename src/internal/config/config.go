1d7e83c2a5cdc5e138d6bf05d1cc399bd8b4afc0
Даниил <dan.demin@gmail.com>
2022-12-03 19:58:14 +0300

    add method to init config; change struct tags

--- src/internal/config/config.go
+++ src/internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	log "github.com/sirupsen/logrus"
+)
+
 var GlobalConfig Config = Config{
 	DB: DBConfig{
 		PostgreSQLDSN: "postgres://user:password@localhost:5432/db?sslmode=disable",
@@ -18,11 +24,32 @@ type Config struct {
 
 type (
 	DBConfig struct {
-		PostgreSQLDSN string `env:"BWG_APP_POSTGRESQL_DSN"`
+		PostgreSQLDSN string `envconfig:"BWG_APP_POSTGRESQL_DSN"`
 	}
 
 	AppConfig struct {
 		// Port is the port on which the server will listen
-		Port string `env:"BWG_APP_PORT"`
+		Port string `envconfig:"BWG_APP_PORT"`
 	}
 )
+
+func InitGlobalConfig() {
+	if err := godotenv.Load("./.env"); err != nil {
+		log.Info("Don't find .env file")
+	}
+
+	if err := envconfig.Process("bwg_app", &GlobalConfig); err != nil {
+		log.WithFields(
+			log.Fields{
+				"function": "envconfig.Process",
+				"error":    err,
+			},
+		).Info("Can't read env vars")
+	}
+
+	log.WithFields(
+		log.Fields{
+			"from": "InitGlobalConfig",
+		},
+	).Infof("config: %+v", GlobalConfig)
+}
